8baf61f0d6107dd590d683a078a1f07d33f3890c
ryantownshend <ryan@discoverygarden.ca>
2018-03-02 09:12:36 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,15 @@ from __future__ import with_statement
 from setuptools import setup
 
 setup(
-    name='honk',
-    version='0.5',
-    description='Tool for dumping output of HTTP requests',
+    name='scid',
+    version='0.1',
+    description='Tool for installing solr config files',
     url='https://github.com/ryantownshend/solr_config_installer_device',
     author='Ryan Townshend',
     author_email='citizen.townshend@gmail.com',
     install_requires=[
         'click>=6.7',
         'click-log>=0.2.1',
-        'PyYAML>=3.12',
-        'tabulate>=0.8.2'
     ],
     py_modules=['honk'],
     entry_points={
